1abfcd71e52b17c5172e6f397dbdcc965d3a9cd3
creatorfromhell <daniel.viddy@gmail.com>
2019-12-13 19:41:48 -0500

    ItemStackBuilder

--- src/net/tnemc/plugincore/core/managers/WorldManager.java
+++ src/net/tnemc/plugincore/core/managers/WorldManager.java
@@ -1,5 +1,10 @@
 package net.tnemc.plugincore.core.managers;
 
+import net.tnemc.plugincore.core.world.WorldConnectionProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * The WorldManager class is used to store connections between various worlds. It allows you to directly
  * store what world name should be returned for each world name request, i.e. if you have a feature
@@ -9,6 +14,8 @@ package net.tnemc.plugincore.core.managers;
  * would be linked to "world" in the economy service, but have no link under the "statistics" service.
  */
 public class WorldManager {
+
+  private Map<String, WorldConnectionProvider> providers = new HashMap<>();
 
   private String defaultWorld = "world";
 
