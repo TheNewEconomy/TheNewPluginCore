b6c3df137526cda5385f50687e1ddfb7092e4c3d
creatorfromhell <daniel.viddy@gmail.com>
2024-02-03 19:02:11 -0500

    Make BukkitCore abstract.

--- TNPC-Bukkit/src/net/tnemc/plugincore/bukkit/BukkitPluginCore.java
+++ TNPC-Bukkit/src/net/tnemc/plugincore/bukkit/BukkitPluginCore.java
@@ -22,12 +22,9 @@ import net.tnemc.plugincore.PluginCore;
 import net.tnemc.plugincore.bukkit.impl.BukkitLogProvider;
 import net.tnemc.plugincore.bukkit.impl.BukkitServerProvider;
 import net.tnemc.plugincore.core.api.CallbackProvider;
-import net.tnemc.plugincore.core.api.callback.Callback;
 import net.tnemc.plugincore.core.io.message.TranslationProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import revxrsal.commands.bukkit.BukkitCommandHandler;
-import revxrsal.commands.command.CommandActor;
-import revxrsal.commands.command.ExecutableCommand;
 
 /**
  * BukkitPluginCore
@@ -35,9 +32,9 @@ import revxrsal.commands.command.ExecutableCommand;
  * @author creatorfromhell
  * @since 0.0.1.0
  */
-public class BukkitPluginCore extends PluginCore {
+public abstract class BukkitPluginCore extends PluginCore {
 
-  private JavaPlugin plugin;
+  private final JavaPlugin plugin;
   public BukkitPluginCore(JavaPlugin plugin, TranslationProvider provider, CallbackProvider callbackProvider) {
     super(new BukkitServerProvider(), new BukkitLogProvider(plugin.getLogger()), provider,
             callbackProvider);
@@ -66,55 +63,8 @@ public class BukkitPluginCore extends PluginCore {
   }
 
   @Override
-  public void registerConfigs() {
-
-  }
-
-  @Override
   public void registerMenuHandler() {
     this.menuHandler = new BukkitMenuHandler(plugin, true);
-  }
-
-  @Override
-  public void registerStorage() {
-
-  }
-
-  @Override
-  public String commandHelpWriter(ExecutableCommand command, CommandActor actor) {
-    return null;
-  }
-
-  /**
-   * Used to register commands.
-   */
-  @Override
-  public void registerCommands() {
-
-  }
-
-  /**
-   * Used to register {@link Callback Callbacks} during initialization.
-   */
-  @Override
-  public void registerCallbacks() {
-
-  }
-
-  /**
-   * @return The current version for this plugin.
-   */
-  @Override
-  public String version() {
-    return null;
-  }
-
-  /**
-   * @return The build for this version.
-   */
-  @Override
-  public String build() {
-    return null;
   }
 
   public static BukkitPluginCore instance() {
